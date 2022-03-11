e8c7fbd7cb9711f74de839666b5202bebc561bbd
calviar-odoo <calviarpayall@gmail.com>
2022-03-11 18:42:33 +0000

    fix indent

--- odoo_academy/models/sale_subs_exercise.py
+++ odoo_academy/models/sale_subs_exercise.py
@@ -5,26 +5,27 @@ from dateutil.relativedelta import relativedelta
 class SubsExercise(models.Model):
     _inherit = 'sale.subscription'
     
-def _prepare_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
-        company = self.env.company or line.analytic_account_id.company_id
-        tax_ids = line.product_id.taxes_id.filtered(lambda t: t.company_id == company)
-        price_unit = line.price_unit
-        if fiscal_position and tax_ids:
-            tax_ids = self.env['account.fiscal.position'].browse(fiscal_position).map_tax(tax_ids)
-            price_unit = self.env['account.tax']._fix_tax_included_price_company(line.price_unit, line.product_id.taxes_id, tax_ids, self.company_id)
-        return {
-            'name': 'NOMBRE NUEVO',
-            'subscription_id': line.analytic_account_id.id,
-            'price_unit': price_unit or 0.0,
-            'discount': line.discount,
-            'quantity': line.quantity,
-            'product_uom_id': line.uom_id.id,
-            'product_id': line.product_id.id,
-            'tax_ids': [(6, 0, tax_ids.ids)],
-            'analytic_account_id': line.analytic_account_id.analytic_account_id.id,
-            'analytic_tag_ids': [(6, 0, line.analytic_account_id.tag_ids.ids)],
-            'subscription_start_date': date_start,
-            'subscription_end_date': date_stop,
+    @api.model
+    def _prepare_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
+            company = self.env.company or line.analytic_account_id.company_id
+            tax_ids = line.product_id.taxes_id.filtered(lambda t: t.company_id == company)
+            price_unit = line.price_unit
+            if fiscal_position and tax_ids:
+                tax_ids = self.env['account.fiscal.position'].browse(fiscal_position).map_tax(tax_ids)
+                price_unit = self.env['account.tax']._fix_tax_included_price_company(line.price_unit, line.product_id.taxes_id, tax_ids, self.company_id)
+            return {
+                'name': 'NOMBRE NUEVO',
+                'subscription_id': line.analytic_account_id.id,
+                'price_unit': price_unit or 0.0,
+                'discount': line.discount,
+                'quantity': line.quantity,
+                'product_uom_id': line.uom_id.id,
+                'product_id': line.product_id.id,
+                'tax_ids': [(6, 0, tax_ids.ids)],
+                'analytic_account_id': line.analytic_account_id.analytic_account_id.id,
+                'analytic_tag_ids': [(6, 0, line.analytic_account_id.tag_ids.ids)],
+                'subscription_start_date': date_start,
+                'subscription_end_date': date_stop,
         }
 
     def _prepare_invoice_lines(self, fiscal_position):
