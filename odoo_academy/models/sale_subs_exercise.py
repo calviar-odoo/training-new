df454436216ef60c6536731df2e6a29acfa88be7
calviar-odoo <calviarpayall@gmail.com>
2022-03-14 14:17:51 +0000

    try fix array syntax

--- odoo_academy/models/sale_subs_exercise.py
+++ odoo_academy/models/sale_subs_exercise.py
@@ -35,15 +35,15 @@ class SubsExercise(models.Model):
             price_unit = self.env['account.tax']._fix_tax_included_price_company(line.price_unit, line.product_id.taxes_id, tax_ids, self.company_id)
         return {
             'name': 'NuevoProducto',
-            'subscription_id': line.analytic_account_id.id,
-            'price_unit': price_unit or 0.0,
-            'discount': line.discount,
-            'quantity': line.quantity,
-            'product_uom_id': line.uom_id.id,
-            'product_id': line.product_id.id,
-            'tax_ids': [(6, 0, tax_ids.ids)],
-            'analytic_account_id': line.analytic_account_id.analytic_account_id.id,
-            'analytic_tag_ids': [(6, 0, line.analytic_account_id.tag_ids.ids)],
+            'subscription_id': 10,
+            'price_unit': 10,
+            'discount': 10,
+            'quantity': 10,
+            'product_uom_id': 10,
+            'product_id': 10,
+            'tax_ids': 10,
+            'analytic_account_id': 10,
+            'analytic_tag_ids': 10,
             'subscription_start_date': date_start,
             'subscription_end_date': date_stop,
         }
@@ -52,7 +52,7 @@ class SubsExercise(models.Model):
         self.ensure_one()
         revenue_date_start = self.recurring_next_date
         revenue_date_stop = revenue_date_start + relativedelta(**{PERIODS[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
-        return [((0, 0, self._prepare_invoice_extra_line(line, fiscal_position, revenue_date_start, revenue_date_stop))),
+        return [((0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))),
             ((0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids)]
     
   #  (0, 0, self._prepare_invoice_extra_line(line, fiscal_position, revenue_date_start, revenue_date_stop)),
