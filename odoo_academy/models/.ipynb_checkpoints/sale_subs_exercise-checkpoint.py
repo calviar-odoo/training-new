9e705e15ead6104918dacb4c15b785692419206a
calviar-odoo <calviarpayall@gmail.com>
2022-03-17 15:10:57 +0000

    funcionaporfis

--- odoo_academy/models/.ipynb_checkpoints/sale_subs_exercise-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/sale_subs_exercise-checkpoint.py
@@ -27,12 +27,7 @@ PERIODS = {'daily': 'days', 'weekly': 'weeks', 'monthly': 'months', 'yearly': 'y
 class SubsExercise(models.Model):
     _inherit = 'sale.subscription'
 
-    
-    
-    def _prepare_invoice_lines(self, fiscal_position):
-        self.ensure_one()
-        revenue_date_start = self.recurring_next_date
-        revenue_date_stop = revenue_date_start + relativedelta(**{PERIODS[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
+    def _prepare_invoice_extra_line(self, fiscal_position, date_start=False, date_stop=False):
         productos = [
             {
             'name': 'PRODUCTO2',
@@ -56,6 +51,13 @@ class SubsExercise(models.Model):
             'subscription_end_date': date_stop,
             }
         ]
+    
+    def _prepare_invoice_lines(self, fiscal_position):
+        self.ensure_one()
+        revenue_date_start = self.recurring_next_date
+        revenue_date_stop = revenue_date_start + relativedelta(**{PERIODS[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
         return [(0, 0, productos)]
-        #return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]            
+        #return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]
+        #return [(0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))),
+              
 # New COmment
