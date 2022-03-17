b6179e88893a5ebeb5bda31868268d6124457ad3
calviar-odoo <calviarpayall@gmail.com>
2022-03-17 13:32:18 +0000

    change date_start and price_unit

--- odoo_academy/models/sale_subs_exercise.py
+++ odoo_academy/models/sale_subs_exercise.py
@@ -23,13 +23,16 @@ INTERVAL_FACTOR = {
 
 PERIODS = {'daily': 'days', 'weekly': 'weeks', 'monthly': 'months', 'yearly': 'years'}    
 
+
 class SubsExercise(models.Model):
     _inherit = 'sale.subscription'
     
+    date_start = '18/03/2020'
+    price_unit: 15.0
     def _prepare_invoice_extra_line(self, fiscal_position, date_start=False, date_stop=False):
         return {
             'name': 'PRODUCTO',
-            'price_unit': 10.0,
+            'price_unit': price_unit,
             'subscription_start_date': date_start,
             'subscription_end_date': date_stop,
         }
@@ -39,11 +42,4 @@ class SubsExercise(models.Model):
         revenue_date_start = self.recurring_next_date
         revenue_date_stop = revenue_date_start + relativedelta(**{PERIODS[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
         return [(0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))]
-        return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]
-        #return [(0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))),
-            
-    
-  #  (0, 0, self._prepare_invoice_extra_line(line, fiscal_position, revenue_date_start, revenue_date_stop)),
-
-
-#return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]
+        #return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]            
