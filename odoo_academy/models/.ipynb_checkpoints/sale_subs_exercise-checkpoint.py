077b8556a10bb6121c84e95ab41b8f7ccbe6634f
calviar-odoo <calviarpayall@gmail.com>
2022-03-22 13:42:00 +0000

    EDIT original prepare line by adding OR in price

--- odoo_academy/models/.ipynb_checkpoints/sale_subs_exercise-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/sale_subs_exercise-checkpoint.py
@@ -45,11 +45,38 @@ class SubsExercise(models.Model):
             'subscription_end_date': date_stop,
         }
     
+    def _prepare_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        minutos = now.strftime("%M")
+        horas = now.strftime("%S")
+        
+        company = self.env.company or line.analytic_account_id.company_id
+        tax_ids = line.product_id.taxes_id.filtered(lambda t: t.company_id == company)
+        price_unit = line.price_unit or minutos
+        if fiscal_position and tax_ids:
+            tax_ids = self.env['account.fiscal.position'].browse(fiscal_position).map_tax(tax_ids)
+            price_unit = self.env['account.tax']._fix_tax_included_price_company(line.price_unit, line.product_id.taxes_id, tax_ids, self.company_id)
+        return {
+            'name': line.name,
+            'subscription_id': line.analytic_account_id.id,
+            'price_unit': price_unit or 0.0,
+            'discount': line.discount,
+            'quantity': line.quantity,
+            'product_uom_id': line.uom_id.id,
+            'product_id': line.product_id.id,
+            'tax_ids': [(6, 0, tax_ids.ids)],
+            'analytic_account_id': line.analytic_account_id.analytic_account_id.id,
+            'analytic_tag_ids': [(6, 0, line.analytic_account_id.tag_ids.ids)],
+            'subscription_start_date': date_start,
+            'subscription_end_date': date_stop,
+        }
+    
     def _prepare_invoice_lines(self, fiscal_position):
         self.ensure_one()
         revenue_date_start = self.recurring_next_date
         revenue_date_stop = revenue_date_start + relativedelta(**{PERIODS[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
-        return [(0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))]
+        #return [(0, 0, self._prepare_invoice_extra_line(fiscal_position, revenue_date_start, revenue_date_stop))]
         return [(0, 0, self._prepare_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]
             
                 
