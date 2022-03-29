4d82389f7d98122b368aef75cc61fc7a38bb4a68
calviar-odoo <calviarpayall@gmail.com>
2022-03-29 19:10:48 +0000

    ADDED model Spaceship

---
+++ odoo_space/models/spaceship.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields, api
+
+class Spaceship(models.Model):
+    
+    _name = 'odoospace.spaceship'
+    _description = 'Spaceships Description'
