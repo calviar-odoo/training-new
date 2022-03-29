19f434da5adbfe89c65b2aba75c6c85f4fa8e034
calviar-odoo <calviarpayall@gmail.com>
2022-03-29 18:39:10 +0000

    Initial Space Commit

---
+++ odoo_space/manifest.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space',
+    
+    'summary': """App to get to space succesfully""",
+    
+    'description': """
+        Odoo Spatial Mission, the tripulation needs to arrive to the moon
+    """,
+    
+    'author': 'Cesaro',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'], # Añadido "account_move"
+    
+    'data': [
+
+    ],
+    
+    'demo': [
+
+    ],
+}
